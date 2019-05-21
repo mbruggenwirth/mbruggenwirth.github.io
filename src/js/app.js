68f148ee275a31cf105067a4822221cd85652088
Michiel Bruggenwirth <mbruggenwirth@gmail.com>
2019-05-21 23:47:31 +0200

    code cleanup

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,30 @@
 import "../scss/main.scss";
 
-const grid = 3;
-const matrix = [...Array(grid)].map(x => Array(grid).fill(0));
-const squares = matrix;
-const game = document.getElementById('game');
-const gameSegment = document.createElement('div');
-const scoreSegment = document.createElement('div');
-gameSegment.classList.add('grid');
-scoreSegment.classList.add('scores');
-game.appendChild(gameSegment);
-game.appendChild(scoreSegment);
-
 function loopIndex(array, current, step) {
     const nextIndex = current + step;
     return nextIndex >= array.length ? nextIndex - array.length : nextIndex;
 }
 
-
-
 class Player {
-    constructor() {
+    constructor(scoreDOM) {
         this.score = 0;
+        this.player = {
+            name: 'Michiel',
+            color: "#ff000"
+        };
+        this.DOM = scoreDOM;
+        this.renderScore();
     }
 
     increaseScore() {
         this.score = this.score + 1;
+        this.renderScore();
+    }
+
+    renderScore() {
+        this.DOM.textContent = this.score.toString();
     }
 }
-
 
 
 class Square {
@@ -38,6 +35,7 @@ class Square {
         this.col = col;
         this.positions = ['top', 'right', 'bottom', 'left'];
         this.renderSquare();
+        this.neighbours = {};
     }
 
     renderSquare() {
@@ -62,17 +60,16 @@ class Square {
         this.setActiveLine(line);
 
         if(this.neighbours[position]) {
-            this.handleNeigbour(position);
+            this.handleNeighbour(position);
         }
 
         const win = this.checkWinner(this);
         if(!win) {
-            activePlayer = loopIndex(players, activePlayer, 1);
-            console.log(players[activePlayer]);
+            game.activePlayer = loopIndex(game.players, game.activePlayer, 1);
         }
     }
 
-    handleNeigbour(position) {
+    handleNeighbour(position) {
         const opposite = loopIndex(this.positions, this.positions.indexOf(position), 2);
         this.setActiveLine(this.neighbours[position].lines[opposite]);
         this.checkWinner(this.neighbours[position]);
@@ -81,8 +78,8 @@ class Square {
     checkWinner(square) {
         const winner = square.lines.filter(line => line.classList.contains('set')).length === 4;
         if(winner) {
-            square.square.classList.add(`win-player-${activePlayer}`);
-            players[activePlayer].increaseScore();
+            square.square.classList.add(`win-player-${game.activePlayer}`);
+            game.players[game.activePlayer].increaseScore();
             return true;
         }
         return false;
@@ -91,41 +88,86 @@ class Square {
     setActiveLine(line) {
         line.classList.add('set');
     }
+}
 
-    getSquare(row, col) {
-        if(squares[row] !== undefined && squares[row][col] !== undefined) {
-            return squares[row][col];
+
+class Game {
+    constructor(options) {
+        const defaultOptions = {
+            grid: 3,
+            players: 2
+        };
+        this.options = Object.assign({}, defaultOptions, options);
+        this.grid = this.options.grid ? this.options.grid : 3;
+        this.matrix = [...Array(this.grid)].map(x => Array(this.grid).fill(0));
+        this.squares = this.matrix;
+        this.createBoard();
+        this.createGrid();
+        this.setNeighbours();
+
+        this.activePlayer = 0;
+        this.players = this.setPlayers(this.options.players);
+    }
+
+    createBoard() {
+        const game = document.getElementById('game');
+        this.gameBoard = document.createElement('div');
+        this.scoreBoard = document.createElement('div');
+        this.gameBoard.classList.add('grid');
+        this.scoreBoard.classList.add('scores');
+        game.appendChild(this.gameBoard);
+        game.appendChild(this.scoreBoard);
+    }
+
+    createGrid(){
+        this.matrix.map((rows, rowIndex) => {
+            const row = document.createElement('div');
+            row.classList.add('row');
+            rows.map((column, columnIndex) => {
+                const square = new Square(rowIndex, columnIndex);
+                this.squares[rowIndex][columnIndex] = square;
+                row.appendChild(square.renderSquare());
+                this.gameBoard.appendChild(row);
+            });
+        });
+    }
+
+    setNeighbours() {
+        this.matrix.forEach((rows, rowIndex) => {
+            rows.forEach((column, columnIndex) => {
+                this.findNeighboursOfSquare(this.squares[rowIndex][columnIndex]);
+            })
+        });
+    }
+
+    setPlayers(amount) {
+        return [...Array(amount)].map(x => {
+            const scoreElement = document.createElement('span');
+            this.scoreBoard.append(scoreElement);
+            return new Player(scoreElement);
+        });
+    }
+
+    findSquareOnPosition(row, col) {
+        if(this.squares[row] !== undefined && this.squares[row][col] !== undefined) {
+            return this.squares[row][col];
         }
     }
 
-    getNeighbours() {
-        this.neighbours = {
-            top: this.getSquare(this.row - 1, this.col),
-            right: this.getSquare(this.row, this.col + 1),
-            bottom: this.getSquare(this.row + 1, this.col),
-            left: this.getSquare(this.row, this.col - 1)
+    findNeighboursOfSquare(square) {
+        square.neighbours = {
+            top: this.findSquareOnPosition(square.row - 1, square.col),
+            right: this.findSquareOnPosition(square.row, square.col + 1),
+            bottom: this.findSquareOnPosition(square.row + 1, square.col),
+            left: this.findSquareOnPosition(square.row, square.col - 1)
         };
     }
 }
 
-let activePlayer = 0;
-const players = [...Array(2)].map(x => {
-    return new Player();
-} );
 
-matrix.map((rows, rowIndex) => {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    rows.map((column, columnIndex) => {
-        const square = new Square(rowIndex, columnIndex);
-        squares[rowIndex][columnIndex] = square;
-        row.appendChild(square.renderSquare());
-        gameSegment.appendChild(row);
-    });
+const game = new Game({
+    grid: 3,
+    players: 2
 });
 
-matrix.map((rows, rowIndex) => {
-    rows.map((column, columnIndex) => {
-        squares[rowIndex][columnIndex].getNeighbours();
-    })
-});
+
